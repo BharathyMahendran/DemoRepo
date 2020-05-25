fff57acbba34772f60b7d69e12bb26d825083540
BharathyMahendrn <bharathysrid@gmail.com>
2020-05-24 22:12:46 -0700

    added search page and update login

--- src/main/java/Com/qa/Pages/LoginPage.java
+++ src/main/java/Com/qa/Pages/LoginPage.java
@@ -1,5 +1,14 @@
 package Com.qa.Pages;
 
 public class LoginPage {
+	
+	public void loginPge() {
+		System.out.println(" Login ..page");
+	}
+	
+	public void loginSet() {
+		
+		System.out.println(" setup");
+	}
 
 }
